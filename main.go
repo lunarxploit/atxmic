c010718c44da199cbef064defe15a330cfde2639
lvncry <142523482+lvncry@users.noreply.github.com>
2023-09-19 08:44:42 +0000

    update

--- main.go
+++ main.go
@@ -6,15 +6,14 @@ import (
 )
 
 func main() {
-	
+
 	http.HandleFunc("/", handler)
 	fmt.Println("Server is running on :8080")
 	http.ListenAndServe(":8080", nil)
-
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, `<h1></h1>`)
+	fmt.Fprintln(w, `<h1>Atxmic is running on :8080</h1>`)
 }
 
 func serveCss(w http.ResponseWriter, r *http.Request) {
