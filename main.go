d50674b3668125cc3f6219fb5b7bb0919d8795e2
lunaralt <10549429-lunaralt@users.noreply.replit.com>
2023-09-19 08:27:27 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.ServeFile()
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, `<h1></h1>`)
+}
+
+func serveCss(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "client/game.css")
+}
